74a8cfb550e9c97de22b82f0eafbf8cb60fe150d
Adam W <developeradamw@gmail.com>
2022-02-03 15:13:39 +0100

    player speed, player can go right

--- src/lucas/hazardous/MainPanel.java
+++ src/lucas/hazardous/MainPanel.java
@@ -20,23 +20,30 @@ public class MainPanel extends JPanel implements ActionListener {
 
     //game map
     private byte[][] map = new byte[][]{
-            {0,0,0,1,1},
-            {0,0,1,1,1},
-            {0,0,1,1,0},
-            {0,0,0,1,1},
-            {1,1,1,1,1}
+            {0, 0, 0, 1, 1},
+            {0, 0, 1, 1, 1},
+            {0, 0, 1, 1, 0},
+            {0, 0, 0, 1, 1},
+            {1, 1, 1, 1, 1}
     };
 
     //player stats
-    private static final int MAX_PLAYER_SPEED = 50;
+    private static final int MAX_PLAYER_SPEED = 1;
     private static final int PLAYER_SIZE = 10;
-    private int currentPlayerSpeed = 0;
-    private int player_direction_x = 0;
-    private int player_direction_y = 0;
-    private static final int PLAYER_DIRECTION_RANGE = 3;
+
     private boolean isDriving = false;
-    private int playerX = 0;
-    private int playerY = GAME_WIDTH-TILE_SIZE/2;
+    private boolean goingRight = false;
+    private boolean goingLeft = false;
+    private boolean goingBack = false;
+
+    private int playerX = 9;
+    private int playerY = GAME_HEIGHT / 2;
+
+    private int speedTime = 0;
+    private static final int TIME_TO_REACH_MAX_SPEED = 7;
+
+    private float directionPlayerY = -1f;
+    private boolean mainDir = true;
 
     MainPanel() {
         this.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
@@ -56,16 +63,16 @@ public class MainPanel extends JPanel implements ActionListener {
     }
 
     private void drawEverything(Graphics g) {
-        if(isGameRunning) {
+        if (isGameRunning) {
             //draw map
-            for(int row = 0; row < map.length; row++) {
-                for(int tile = 0; tile < map[row].length; tile++) {
-                    if(map[row][tile] == 1) {
+            for (int row = 0; row < map.length; row++) {
+                for (int tile = 0; tile < map[row].length; tile++) {
+                    if (map[row][tile] == 1) {
                         g.setColor(Color.gray);
                     } else {
                         g.setColor(Color.green);
                     }
-                    g.fillRect(tile*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                    g.fillRect(tile * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
                 }
             }
 
@@ -76,14 +83,70 @@ public class MainPanel extends JPanel implements ActionListener {
     }
 
     private void playerMove() {
-        if(isDriving) {
+        if (isDriving) {
+            if (goingRight) {
+                if (directionPlayerY <= -1f) {
+                    mainDir = false;
+                }
 
+                if (directionPlayerY >= 1f) {
+                    mainDir = true;
+                }
+
+                if (mainDir) {
+                    directionPlayerY -= .2f;
+                } else {
+                    directionPlayerY += .2f;
+                }
+            }
+
+            if(directionPlayerY > 0f) {
+                if (mainDir) {
+                    playerX += (Math.pow(speedTime, 1.3) * MAX_PLAYER_SPEED) / 2 * -(1f - directionPlayerY);
+                } else {
+                    playerX += (Math.pow(speedTime, 1.3) * MAX_PLAYER_SPEED) / 2 * (1f - directionPlayerY);
+                }
+            } else {
+                if (mainDir) {
+                    playerX += (Math.pow(speedTime, 1.3) * MAX_PLAYER_SPEED) / 2 * -(1f + directionPlayerY);
+                } else {
+                    playerX += (Math.pow(speedTime, 1.3) * MAX_PLAYER_SPEED) / 2 * (1f + directionPlayerY);
+                }
+            }
+
+
+
+            playerY += (Math.pow(speedTime, 1.3) * MAX_PLAYER_SPEED) / 2 * directionPlayerY;
+
+
+            if (playerX < 0) {
+                playerX = 0;
+            }
+            if (playerY < 0) {
+                playerY = 0;
+            }
+
+            if (playerX > GAME_WIDTH - PLAYER_SIZE) {
+                playerX = GAME_WIDTH - PLAYER_SIZE;
+            }
+
+            if (playerY > GAME_HEIGHT - PLAYER_SIZE) {
+                playerY = GAME_HEIGHT - PLAYER_SIZE;
+            }
+
+            if (speedTime < TIME_TO_REACH_MAX_SPEED) {
+                speedTime += 1;
+            }
+        } else {
+            if (speedTime > 0) {
+                speedTime -= 1;
+            }
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(isGameRunning) {
+        if (isGameRunning) {
             playerMove();
         }
         repaint();
@@ -96,14 +159,33 @@ public class MainPanel extends JPanel implements ActionListener {
                 case KeyEvent.VK_W:
                     isDriving = true;
                     break;
+                case KeyEvent.VK_D:
+                    goingRight = true;
+                    break;
+                case KeyEvent.VK_A:
+                    goingLeft = true;
+                    break;
+                case KeyEvent.VK_S:
+                    goingBack = true;
+                    break;
             }
         }
 
         @Override
-        public void keyReleased(KeyEvent e){
+        public void keyReleased(KeyEvent e) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_W:
                     isDriving = false;
+                    speedTime = 0;
+                    break;
+                case KeyEvent.VK_D:
+                    goingRight = false;
+                    break;
+                case KeyEvent.VK_A:
+                    goingLeft = false;
+                    break;
+                case KeyEvent.VK_S:
+                    goingBack = false;
                     break;
             }
         }
