901781957a57951b2d1e479722a0196f0695c735
Adam W <developeradamw@gmail.com>
2022-02-03 12:31:21 +0100

    main frame

--- src/lucas/hazardous/Main.java
+++ src/lucas/hazardous/Main.java
@@ -3,6 +3,6 @@ package lucas.hazardous;
 public class Main {
 
     public static void main(String[] args) {
-
+        new MainFrame();
     }
 }
---
+++ src/lucas/hazardous/MainFrame.java
@@ -0,0 +1,14 @@
+package lucas.hazardous;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    MainFrame() {
+        this.setTitle("UltraTuxKart");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
