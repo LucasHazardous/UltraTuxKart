bcc86a0dd393d0cd2355acf1f20d0154e26969dc
Adam W <developeradamw@gmail.com>
2022-02-04 23:05:30 +0100

    code organization

--- src/lucas/hazardous/MainFrame.java
+++ src/lucas/hazardous/MainFrame.java
@@ -1,6 +1,6 @@
 package lucas.hazardous;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
     MainFrame() {
--- src/lucas/hazardous/MainPanel.java
+++ src/lucas/hazardous/MainPanel.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,13 +27,15 @@ public class MainPanel extends JPanel implements ActionListener {
     private final List<Integer> mapStartingPoint = new ArrayList<>();
     private final List<Integer> mapTargetPoint = new ArrayList<>();
 
+    //path from mapStartingPoint to mapTargetPoint
     private List<List<Integer>> path;
 
+    //set points for path and calculate path
     {
         mapTargetPoint.add(0);
-        mapTargetPoint.add(map[0].length-1);
+        mapTargetPoint.add(map[0].length - 1);
 
-        mapStartingPoint.add(map.length-1);
+        mapStartingPoint.add(map.length - 1);
         mapStartingPoint.add(0);
 
         path = findPathToTarget(mapTargetPoint, mapStartingPoint, new ArrayList(), new ArrayList());
@@ -46,15 +47,14 @@ public class MainPanel extends JPanel implements ActionListener {
     private final Timer timer;
     private final boolean isGameRunning;
     private final Player player = new Player(GAME_WIDTH, GAME_HEIGHT,
-            mapStartingPoint.get(1)*TILE_SIZE+TILE_SIZE/2,
-            mapStartingPoint.get(0)*TILE_SIZE+TILE_SIZE/2);
-
+            mapStartingPoint.get(1) * TILE_SIZE + TILE_SIZE / 2,
+            mapStartingPoint.get(0) * TILE_SIZE + TILE_SIZE / 2);
 
     MainPanel() {
         this.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
-        this.addKeyListener(new ControllKeyadapter());
+        this.addKeyListener(new ControlKeyAdapter());
 
         isGameRunning = true;
         timer = new Timer(DELAY, this);
@@ -81,9 +81,9 @@ public class MainPanel extends JPanel implements ActionListener {
                 }
             }
 
-            for (List<Integer> point: path) {
+            for (List<Integer> point : path) {
                 g.setColor(Color.cyan);
-                g.fillRect(point.get(1) * TILE_SIZE+TILE_SIZE/2, point.get(0) * TILE_SIZE+TILE_SIZE/2, 2, 2);
+                g.fillRect(point.get(1) * TILE_SIZE + TILE_SIZE / 2, point.get(0) * TILE_SIZE + TILE_SIZE / 2, 2, 2);
             }
 
             //draw player
@@ -98,7 +98,7 @@ public class MainPanel extends JPanel implements ActionListener {
     //find path on map from starting point to target
     private List findPathToTarget(List<Integer> targetPosition, List<Integer> currentPosition, List solution, List visited) {
         //base case - found point is target point
-        if(currentPosition.equals(targetPosition)) {
+        if (currentPosition.equals(targetPosition)) {
             solution.add(currentPosition);
             return solution;
         } else {
@@ -110,10 +110,10 @@ public class MainPanel extends JPanel implements ActionListener {
 
             //all locations that are possible to reach from current point
             int[][] options = new int[][]{
-                    {currentPosition.get(0) +1, currentPosition.get(1)},
-                    {currentPosition.get(0) -1, currentPosition.get(1)},
-                    {currentPosition.get(0), currentPosition.get(1) -1},
-                    {currentPosition.get(0), currentPosition.get(1) +1}
+                    {currentPosition.get(0) + 1, currentPosition.get(1)},
+                    {currentPosition.get(0) - 1, currentPosition.get(1)},
+                    {currentPosition.get(0), currentPosition.get(1) - 1},
+                    {currentPosition.get(0), currentPosition.get(1) + 1}
             };
 
             //store temporary result from recursion
@@ -126,22 +126,22 @@ public class MainPanel extends JPanel implements ActionListener {
             visited.add(currentPosition);
 
             //check every possible location
-            for(int[] option : options) {
-                    currentOptionButList = new ArrayList();
-                    currentOptionButList.add(option[0]);
-                    currentOptionButList.add(option[1]);
+            for (int[] option : options) {
+                currentOptionButList = new ArrayList();
+                currentOptionButList.add(option[0]);
+                currentOptionButList.add(option[1]);
 
-                    //check if location exists, if it's a part if path and was not visited
-                    if((option[0] >= 0) && (option[0] < map.length) && (option[1] >= 0) && (option[1] < map[0].length)
-                            && map[option[0]][option[1]] == 1 && !visited.contains(currentOptionButList)) {
+                //check if location exists, if it's a part if path and was not visited
+                if ((option[0] >= 0) && (option[0] < map.length) && (option[1] >= 0) && (option[1] < map[0].length)
+                        && map[option[0]][option[1]] == 1 && !visited.contains(currentOptionButList)) {
 
-                        tmpResult = findPathToTarget(targetPosition, currentOptionButList, new ArrayList(result), new ArrayList(visited));
+                    tmpResult = findPathToTarget(targetPosition, currentOptionButList, new ArrayList(result), new ArrayList(visited));
 
-                        if(tmpResult.size() < bestResultLength && tmpResult.size() != 0) {
-                            bestResultLength = tmpResult.size();
-                            result = new ArrayList(tmpResult);
-                        }
+                    if (tmpResult.size() < bestResultLength && tmpResult.size() != 0) {
+                        bestResultLength = tmpResult.size();
+                        result = new ArrayList(tmpResult);
                     }
+                }
             }
 
             return result;
@@ -157,7 +157,7 @@ public class MainPanel extends JPanel implements ActionListener {
     }
 
     //player controls
-    private class ControllKeyadapter extends KeyAdapter {
+    private class ControlKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
