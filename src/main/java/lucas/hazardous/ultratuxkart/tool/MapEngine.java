5c48af9168a9ceb7fae9aaf2eacd6433277bd6a5
Adam W <developeradamw@gmail.com>
2022-06-05 21:01:17 +0200

    mapengine converted to record

--- src/main/java/lucas/hazardous/ultratuxkart/tool/MapEngine.java
+++ src/main/java/lucas/hazardous/ultratuxkart/tool/MapEngine.java
@@ -2,14 +2,7 @@ package lucas.hazardous.ultratuxkart.tool;
 
 import java.awt.*;
 
-public class MapEngine {
-    private byte[][] map;
-    private final int TILE_SIZE;
-
-    public MapEngine(byte[][] map, int TILE_SIZE) {
-        this.map = map;
-        this.TILE_SIZE = TILE_SIZE;
-    }
+public record MapEngine(byte[][] map, int TILE_SIZE) {
 
     public void drawMap(Graphics g) {
         for (int row = 0; row < map.length; row++) {
@@ -17,7 +10,7 @@ public class MapEngine {
 
                 if (map[row][tile] == 1)
                     g.setColor(Color.gray);
-                else if(map[row][tile] == 0)
+                else if (map[row][tile] == 0)
                     g.setColor(Color.green);
 
                 g.fillRect(tile * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
