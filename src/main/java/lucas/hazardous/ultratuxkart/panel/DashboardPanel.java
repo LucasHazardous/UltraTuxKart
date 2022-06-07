61f5c08e045bfca14a814ef25f3282b3c148c6c7
Adam W <developeradamw@gmail.com>
2022-06-07 20:09:38 +0200

    draw boosts in dashboard in a box

--- src/main/java/lucas/hazardous/ultratuxkart/panel/DashboardPanel.java
+++ src/main/java/lucas/hazardous/ultratuxkart/panel/DashboardPanel.java
@@ -1,6 +1,6 @@
 package lucas.hazardous.ultratuxkart.panel;
 
-import lucas.hazardous.ultratuxkart.MainFrame;
+import lucas.hazardous.ultratuxkart.Main;
 import lucas.hazardous.ultratuxkart.entity.Player;
 
 import javax.swing.*;
@@ -8,18 +8,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static lucas.hazardous.ultratuxkart.MainFrame.TIMER_DELAY;
-
 public class DashboardPanel extends JPanel implements ActionListener {
-    private final Timer timer;
 
     private final Player player;
+    private final int PLAYER_STARTING_BOOSTS;
 
     public DashboardPanel(Player player) {
         this.player = player;
+        PLAYER_STARTING_BOOSTS = player.getPlayerBoosts();
 
-        this.setPreferredSize(new Dimension(MainFrame.PANEL_WIDTH, MainFrame.DASHBOARD_PANEL_HEIGHT));
-        timer = new Timer(TIMER_DELAY, this);
+        this.setPreferredSize(new Dimension(Main.PANEL_WIDTH, Main.DASHBOARD_PANEL_HEIGHT));
+        Timer timer = new Timer(Main.TIMER_DELAY, this);
         timer.start();
     }
 
@@ -35,10 +34,11 @@ public class DashboardPanel extends JPanel implements ActionListener {
 
     private void drawPlayerStats(Graphics g) {
         g.setColor(Color.black);
-        g.fillRect(0,0,MainFrame.PANEL_WIDTH, MainFrame.DASHBOARD_PANEL_HEIGHT);
+        g.fillRect(0,0,Main.PANEL_WIDTH, Main.DASHBOARD_PANEL_HEIGHT);
+        g.setColor(Color.cyan);
+        g.drawRect(Main.PANEL_WIDTH/5-5, Main.DASHBOARD_PANEL_HEIGHT/2-5, PLAYER_STARTING_BOOSTS*20, 20);
         for(int i = 0; i < player.getPlayerBoosts(); i++) {
-            g.setColor(Color.cyan);
-            g.fillRect(MainFrame.PANEL_WIDTH/5 + 50*i, MainFrame.DASHBOARD_PANEL_HEIGHT/2, 10, 10);
+            g.fillRect(Main.PANEL_WIDTH/5 + 20*i, Main.DASHBOARD_PANEL_HEIGHT/2, 10, 10);
         }
     }
 }
